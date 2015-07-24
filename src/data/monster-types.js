f7f621464736faa854dcda88220e0993872ccc7e
Paul Stahmer <pstahmer25@gmail.com>
2015-07-23 22:20:21 -0500

    added sample monster

--- src/data/monster-types.js
+++ src/data/monster-types.js
@@ -1,18 +1,20 @@
-export default [
-  {
+export default {
+  normal: {
     name: "Normal",
     strength: 1,
     speed: 1,
     defense: 1
-  }, {
+  },
+  speed: {
     name: "Speed",
     strength: .8,
     speed: 1.3,
     defense: .8
-  }, {
+  },
+  power: {
     name: "Power",
     strength: 1.2,
     speed: .8,
     defense: 1.1
   }
-]
+};
--- src/data/sample-monster.js
+++ src/data/sample-monster.js
@@ -5,5 +5,21 @@ export default {
     defense: 10,
     speed: 10,
     magic: 10
-  }
+  },
+  type: "Normal",
+  moves: [
+    {
+      name: "Suicidal Slash",
+      multipliers: [1.1, 1.5],
+      speedModifiers: [1, .8],
+      effects: {
+        self: {
+          defense: .75
+        },
+        target: {
+          defense: 1
+        }
+      }
+    }
+  ]
 }
